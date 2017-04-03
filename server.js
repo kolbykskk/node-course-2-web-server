fa9522acfbdcee996c178d9c608c4f79fbc9080b
Kolby <kolbykalafut@gmail.com>
2017-04-02 22:11:32 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('welcome.hbs', {
+    pageTitle: 'Welcome Page',
+    welcomeMessage: 'Thank you for stopping by!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Oops! There\'s an error!'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
